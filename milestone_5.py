4cff0bded360388ed2a7e0429774e1ebf1f06e38
WillEckersley <will_eckersley@hotmail.co.uk>
2023-10-11 16:10:09 +0100

    Output formatting updated. Small bug fixed

--- milestone_5.py
+++ milestone_5.py
@@ -17,25 +17,23 @@ class Hangman:
             for index, letter in enumerate(self.word): # allows iteration through indexes, facilitating the addition of repeated letters to the guessed list. 
                 if letter == guess:
                     self.word_guessed[index] = guess # if guessed letter is in input word, match the value of that letter to the index it has in the original input word in the guessed list. 
-                    self.word_guessed_string.join([str(char) for char in self.word_guessed]) # string version of the guessed list to compare against original word. Used to trigger win condition when equal to input word. 
+                    self.word_guessed_string = "".join([str(char) for char in self.word_guessed]) # string version of the guessed list to compare against original word. Used to trigger win condition when equal to input word. 
                     print(f"\nGood guess! {guess} is in the word.") # use of '\n' to facilitated better output formatting. 
                     print(f"Word:{self.word_guessed}")
-                    print(f"Letters tried: {self.list_of_guesses}")
         else:
             self.num_lives -= 1
             self.list_of_guesses.append(guess)
             print(f"\nSorry, {guess} is not in the word.") # use of '\n' to facilitated better output formatting. 
-            print(f"Letters tried: {self.list_of_guesses}")
             print(f"You have {self.num_lives} lives left.")
 
     def ask_for_input(self): # method requests input and checks validity.
         while True:
-            guess = input("Please enter a letter:")
+            guess = input("\nPlease enter a letter:")
             if not guess.isalpha() or len(guess) != 1:
-                print("Invalid character. Please enter a single aphabetical character.")
+                print("\nInvalid character. Please enter a single aphabetical character.")
                 break
             elif guess in self.list_of_guesses:
-                print("You already tried that letter!")
+                print("\nYou already tried that letter!")
                 break
             else:
                 self.check_guess(guess)
@@ -44,6 +42,10 @@ class Hangman:
 def play_game(word_list):
     num_lives = 5
     game = Hangman(word_list, num_lives)
+    count = 1
+    while count >= 1:
+        print(f"\nWord:{game.word_guessed}")
+        count -= 1
     while True:
         if game.num_lives == 0:
             print("\nYou lost!")
@@ -52,7 +54,8 @@ def play_game(word_list):
         elif game.num_lives > 0 and game.word != game.word_guessed_string: # alternative conditionals to previous versions checks input word against developing string version of input word created at line 20.
             game.ask_for_input()
         elif game.word == game.word_guessed_string:
-            print("Congratulations, you've won!")
+            print("\nCongratulations, you've won!")
+            print(f"The word was: {game.word_guessed_string}")
             break
 
 play_game(["apple", "banana", "cherry", "damson", "tangerine", "blueberry", "strawberry", "grape", "melon", "lemon", "blackcurrent", "redcurrent", "orange", "blackberry", "lychee"]) # test the code multiple words.
